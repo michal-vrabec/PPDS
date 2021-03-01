5d1d6f02c9f2037696377560d79aa2c3b9575351
Michal Vrabec <michal.vrabec123@gmail.com>
2021-03-01 23:39:57 +0100

    feat: add events solution to cv2

--- cv2/cv2.py
+++ cv2/cv2.py
@@ -8,18 +8,29 @@ class Shared:
         self.semaphores[0].signal(1)
         self.semaphores[1].signal(2)
 
+        self.events = [Event()] * (threads_n + 2)
+        self.events[0].signal()
+        self.events[1].signal()
+
     def count_fibonacci_sequence_semaphores(self, i):
         self.semaphores[i].wait()
         self.semaphores[i + 1].wait()
         self.fibonacci_sequence[i + 2] = self.fibonacci_sequence[i] + self.fibonacci_sequence[i + 1]
         self.semaphores[i + 2].signal(2)
 
+    def count_fibonacci_sequence_events(self, i):
+        self.events[i].wait()
+        self.events[i + 1].wait()
+        self.fibonacci_sequence[i + 2] = self.fibonacci_sequence[i] + self.fibonacci_sequence[i + 1]
+        self.events[i + 2].signal()
+
 
 def count_fibonacci_sequence(shared_object, i):
-    shared_object.count_fibonacci_sequence_semaphores(i)
+    # shared_object.count_fibonacci_sequence_semaphores(i)
+    shared_object.count_fibonacci_sequence_events(i)
 
 
-number_of_threads = 10
+number_of_threads = 20
 shared = Shared(number_of_threads)
 
 threads = [None] * number_of_threads
