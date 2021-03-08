694891c99a405ea167ef4af736b9638ef0097261
Michal Vrabec <michal.vrabec123@gmail.com>
2021-03-08 23:33:59 +0100

    feat: add stop timer

--- cv3/cv3.py
+++ cv3/cv3.py
@@ -1,4 +1,5 @@
 from fei.ppds import Mutex, Semaphore, Thread
+from random import randint
 from time import sleep
 
 
@@ -9,53 +10,77 @@ class Shared:
         self.mutex = Mutex()
         self.buffer = []
 
+        self.items_produced = 0
+        self.stop = False
 
-def produce(shared_object, time):
+
+def produce(shared_object, time_production):
     while True:
-        sleep(time)
+        sleep(time_production)
 
         shared_object.empty.wait()
         shared_object.mutex.lock()
+        if shared_object.stop:
+            shared_object.mutex.unlock()
+            break
 
         shared_object.buffer.append(1)
+        shared_object.items_produced += 1
 
         shared_object.mutex.unlock()
         shared_object.full.signal()
 
 
-def consume(shared_object, time):
+def consume(shared_object, time_consumption):
     while True:
         shared_object.full.wait()
         shared_object.mutex.lock()
+        if shared_object.stop:
+            shared_object.mutex.unlock()
+            break
 
         shared_object.buffer.pop(0)
 
         shared_object.mutex.unlock()
         shared_object.empty.signal()
 
-        sleep(time)
+        sleep(time_consumption)
 
 
-number_of_producers = 5
-number_of_consumers = 5
-production_time = 2
-consumption_time = 2
-capacity = 10
-
-producers = [None] * number_of_producers
-consumers = [None] * number_of_consumers
-shared = Shared(capacity)
+def timer(shared_object, producers_count, consumers_count):
+    sleep(60)
+    shared_object.mutex.lock()
+    shared_object.stop = True
+    shared_object.full.signal(consumers_count)
+    shared_object.empty.signal(producers_count)
+    shared_object.mutex.unlock()
 
 
-for i in range(number_of_producers):
-    producers[i] = Thread(produce, shared, production_time)
+for i in range(120):
+    number_of_producers = randint(1, 10)
+    number_of_consumers = randint(1, 10)
+    production_time = randint(1, 10)
+    consumption_time = randint(1, 10)
+    capacity = randint(1, 10)
 
-for i in range(number_of_consumers):
-    consumers[i] = Thread(consume, shared, consumption_time)
+    producers = [None] * number_of_producers
+    consumers = [None] * number_of_consumers
+    shared = Shared(capacity)
 
-for producer in producers:
-    producer.join()
+    for j in range(number_of_producers):
+        producers[j] = Thread(produce, shared, production_time)
 
-for consumer in consumers:
-    consumer.join()
+    for j in range(number_of_consumers):
+        consumers[j] = Thread(consume, shared, consumption_time)
+
+    timer_thread = Thread(timer, shared, number_of_producers, number_of_consumers)
+
+    timer_thread.join()
+    for producer in producers:
+        producer.join()
+
+    for consumer in consumers:
+        consumer.join()
+
+    print('finished 1')
 
