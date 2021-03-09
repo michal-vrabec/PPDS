90bc42ef321596a6ca39e4ba96f044fb2b51bc8e
Michal Vrabec <michal.vrabec123@gmail.com>
2021-03-09 01:55:30 +0100

    feat: add data gathering for optimization

--- cv3/cv3.py
+++ cv3/cv3.py
@@ -1,6 +1,7 @@
 from fei.ppds import Mutex, Semaphore, Thread
 from random import randint
 from time import sleep
+import pandas as pd
 
 
 class Shared:
@@ -11,6 +12,7 @@ class Shared:
         self.buffer = []
 
         self.items_produced = 0
+        self.items_consumed = 0
         self.stop = False
 
 
@@ -40,6 +42,7 @@ def consume(shared_object, time_consumption):
             break
 
         shared_object.buffer.pop(0)
+        shared_object.items_consumed += 1
 
         shared_object.mutex.unlock()
         shared_object.empty.signal()
@@ -47,40 +50,77 @@ def consume(shared_object, time_consumption):
         sleep(time_consumption)
 
 
-def timer(shared_object, producers_count, consumers_count):
+def timer(shared_object, producers_count, consumers_count, combination_id):
+    global repetition_produced
+    global repetition_consumed
     sleep(60)
     shared_object.mutex.lock()
     shared_object.stop = True
+
+    repetition_produced[combination_id].append(shared_object.items_produced)
+    repetition_consumed[combination_id].append(shared_object.items_consumed)
+
     shared_object.full.signal(consumers_count)
     shared_object.empty.signal(producers_count)
     shared_object.mutex.unlock()
 
 
-for i in range(120):
+cycle_max = 110
+producers = [None] * cycle_max
+consumers = [None] * cycle_max
+shared = [None] * cycle_max
+timer_threads = [None] * cycle_max
+repetition_produced = [[] for _ in range(cycle_max)]
+repetition_consumed = [[] for _ in range(cycle_max)]
+df = pd.DataFrame(columns=['producers', 'consumers', 'production_time',
+                           'consumption_time', 'capacity', 'produced', 'consumed'])
+
+for i in range(cycle_max):
     number_of_producers = randint(1, 10)
     number_of_consumers = randint(1, 10)
     production_time = randint(1, 10)
     consumption_time = randint(1, 10)
     capacity = randint(1, 10)
 
-    producers = [None] * number_of_producers
-    consumers = [None] * number_of_consumers
-    shared = Shared(capacity)
+    df.loc[i] = [number_of_producers, number_of_consumers, production_time, consumption_time,
+                 capacity, 0, 0]
 
-    for j in range(number_of_producers):
-        producers[j] = Thread(produce, shared, production_time)
 
-    for j in range(number_of_consumers):
-        consumers[j] = Thread(consume, shared, consumption_time)
+for _ in range(10):
+    for i in range(cycle_max):
+        number_of_producers = df.at[i, 'producers']
+        number_of_consumers = df.at[i, 'consumers']
+        production_time = df.at[i, 'production_time']
+        consumption_time = df.at[i, 'consumption_time']
+        capacity = df.at[i, 'capacity']
 
-    timer_thread = Thread(timer, shared, number_of_producers, number_of_consumers)
+        producers[i] = [None] * number_of_producers
+        consumers[i] = [None] * number_of_consumers
+        shared[i] = Shared(capacity)
 
-    timer_thread.join()
-    for producer in producers:
-        producer.join()
+        for j in range(number_of_producers):
+            producers[i][j] = Thread(produce, shared[i], production_time)
 
-    for consumer in consumers:
-        consumer.join()
+        for j in range(number_of_consumers):
+            consumers[i][j] = Thread(consume, shared[i], consumption_time)
 
-    print('finished 1')
+        timer_threads[i] = Thread(timer, shared[i], number_of_producers, number_of_consumers, i)
+        df.loc[i] = [number_of_producers, number_of_consumers, production_time, consumption_time,
+                     capacity, 0, 0]
 
+    for timer_thread in timer_threads:
+        timer_thread.join()
+
+    for producer_i in producers:
+        for producer in producer_i:
+            producer.join()
+
+    for consumer_i in consumers:
+        for consumer in consumer_i:
+            producer.join()
+
+for i in range(cycle_max):
+    df.at[i, 'produced'] = round(sum(repetition_produced[i]) / len(repetition_produced[i]), 2)
+    df.at[i, 'consumed'] = round(sum(repetition_consumed[i]) / len(repetition_consumed[i]), 2)
+
+df.to_csv('data.csv', index=False)
